2f68dbdbef3fa132048acdaa9ac0bab2346ee504
Ivan Dorofenko <linik.katon@gmail.com>
2024-06-12 17:31:33 +0400

    Start Commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Pytho Control Work 12.06.2024
+
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+b = ['a', 'b', 'c', 'd', 'f', 'g', 'k', 'l']
+c = ''
+
+for number, letter in zip(a, b):
+    c += str(number) + ' ' + letter + ' '
+
+print(c)
